9eebd410c1c43564c0ca6aa1812a33cd1f90f3c1
Richard Walker <richard.walker.90@gmail.com>
2018-06-08 20:57:23 -0400

    finish the help for the esclu

---
+++ esclu/index.js
@@ -0,0 +1,25 @@
+'use strict';
+
+const program = require('commander');
+const pkg = require('./package.json');
+
+program
+    .version(pkg.version)
+    .description(pkg.description)
+    .usage('[options] <command> [...]')
+    .option('-o, --host <hostname>', 'hostname [localhost]', 'localhost')
+    .option('-p, --port <number>', 'port number [9200]', '9200')
+    .option('-j, --json', 'format output as JSON')
+    .option('-i, --index <name>', 'which index to use')
+    .option('-t, --type <type>', 'default type for bulk operations');
+
+program.parse(process.argv);
+
+// If there are no arguments then display the help message
+if (!program.args.filter(arg => typeof arg === 'object').length){
+    program.help();
+}
+    
+
+
+
