11e4b2d48960126b860702920d2446b76a829f7c
Richard Walker <richard.walker.90@gmail.com>
2018-06-11 18:42:25 -0400

    the program is not recognizing the url command

--- esclu/index.js
+++ esclu/index.js
@@ -12,13 +12,35 @@ program
     .option('-j, --json', 'format output as JSON')
     .option('-i, --index <name>', 'which index to use')
     .option('-t, --type <type>', 'default type for bulk operations');
-
+    
 program.parse(process.argv);
 
 // If there are no arguments then display the help message
 if (!program.args.filter(arg => typeof arg === 'object').length){
     program.help();
 }
+
+program
+    .command('url [path]')
+    .description('generate the URL for the options and path (default is /)')
+    .action((path = '/') => console.log(fullUrl(path)));
+
+const fullUrl = (path = '') => {
+    let url = 'http://${program.host}:${program.port}/';
+    if (program.index){
+        url += program.index + '/';
+        if(program.type){
+            url += program.type + '/';
+        }
+    }
+    return url + path.replace(/^\/*/g, ''); //The regex strips the first forward slash
+};
+
+
+
+
+
+
     
 
 
